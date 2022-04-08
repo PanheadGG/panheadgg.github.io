2574b2b0c0520698964dace0c189e082f87b9ced
PanheadGG <85760045+PanheadGG@users.noreply.github.com>
2022-04-08 13:37:40 +0800

    Create changetheme.js

---
+++ js/changetheme.js
@@ -0,0 +1,12 @@
+function changetheme(){
+	if(document.getElementById('changetheme').innerHTML===todark){
+		document.body.style.background='#555';
+		document.body.style.color='white';
+		document.getElementById('changetheme').innerHTML=tolight;
+	}
+	else{
+		document.body.style.background='white';
+		document.body.style.color='#555';
+		document.getElementById('changetheme').innerHTML=todark;
+	}
+}
