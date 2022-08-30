c4eb78e5ad7ff9467746cf58cca93f0be886f8a5
PanheadGG <85760045+PanheadGG@users.noreply.github.com>
2022-08-30 09:51:34 +0800

    Create config.js

---
+++ article/how-to-get-bilibili-vip/config.js
@@ -0,0 +1,15 @@
+var playerConfig = {
+    container:'#mui-player',
+    title:'如何获得B站大会员',
+    src:'https://panheadgg.github.io/rickroll/rickroll.m3u8',
+    poster:'https://muiplayer.oss-cn-shanghai.aliyuncs.com/static/image/poster.jpg',
+    videoAttribute:[
+        {attrKey:'webkit-playsinline',attrValue:''},
+        {attrKey:'playsinline',attrValue:''},
+        {attrKey:'x5-playsinline',attrValue:''},
+        {attrKey:'t7-video-player-type',attrValue:'inline'},
+        {attrKey:'x5-video-player-type',attrValue:'h5-page'},
+        {attrKey:'x-webkit-airplay',attrValue:'allow'},
+        {attrKey:'controlslist',attrValue:'nodownload'},
+    ],
+}
